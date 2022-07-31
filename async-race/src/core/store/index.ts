ea6a19e9dd58bbe510bf413a7e2b7d943bfb0520
Jaslan <Jaslan.dlnv@gmail.com>
2022-08-01 00:29:32 +0500

    refactor: change methods in store

--- async-race/src/core/store/index.ts
+++ async-race/src/core/store/index.ts
@@ -1,15 +1,37 @@
+import type { ICar } from '@core/ts/interfaces';
 import EventObserver from '@core/eventObserver';
 
+type TStore = ICar | HTMLDivElement | HTMLInputElement;
 class Store {
-  static store: Map<string, EventObserver<unknown>> = new Map();
-  static getStore(): Map<string, EventObserver<unknown>> {
-    return Store.store;
+  static event: Map<string, EventObserver<unknown>> = new Map();
+  static store: Map<string, TStore> = new Map();
+  static currentPage: number = 1;
+
+  static getCurrentPage(): number {
+    return Store.currentPage;
   }
-  static addToStore(key: string, value: EventObserver<unknown>) {
+  static setCurrentPage(page: number): void {
+    Store.currentPage = page;
+  }
+
+  static addToStore(key: string, value: TStore) {
     Store.store.set(key, value);
   }
-  static remove(key: string): void {
+  static removeFromStore(key: string) {
     Store.store.delete(key);
+  }
+  static getFromStore(key: string): TStore | undefined {
+    return Store.store.get(key);
+  }
+
+  static getEvent(): Map<string, EventObserver<unknown>> {
+    return Store.event;
+  }
+  static addToEvent(key: string, value: EventObserver<unknown>) {
+    Store.event.set(key, value);
+  }
+  static removeEvent(key: string): void {
+    Store.event.delete(key);
   }
 }
 
